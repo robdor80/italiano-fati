8c5ae9e1367ac2abb8d392613e9cdf408d2a019d
robdor80 <ROB.DOR.80@GMAIL.COM>
2025-07-06 22:14:07 +0200

    v23

--- js/firebase/config.js
+++ js/firebase/config.js
@@ -1,11 +1,9 @@
-// firebase/config.js
+// js/firebase/config.js
 
-// Importa Firebase desde los SDK compatibles
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Configuración de tu proyecto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBCzpHpbGPV_KwStxzIk30IutH2G6c00M4",
   authDomain: "italiano-fati.firebaseapp.com",
@@ -15,10 +13,8 @@ const firebaseConfig = {
   appId: "1:1040989423439:web:a2adc758374cf1b906a73a"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Exportar para usar en otros archivos
-export { auth, db };
+export { auth, db, firebaseConfig };
